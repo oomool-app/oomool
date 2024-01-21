ba3257fedc6b9b92262515a609a1967ccc9eeb21
itsmo1031 <itsmo1031@gmail.com>
2024-01-21 21:49:59 +0900

    ✨ feat: setup spring boot

---
+++ backend/src/test/java/com/oomool/api/OomoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oomool.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OomoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
