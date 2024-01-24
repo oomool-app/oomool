8ff293b47d9b22139a8cc8a45a6b863a80e4b382
김성수 <zxzxzlxlzlxl@naver.com>
2024-01-24 17:58:07 +0900

    ✨ feat: eccessToken을 사용하여 사용자 정보 가져오기
    eccessToken을 사용하여 사용자 정보를 가져오는 메서드를 생성했다(createKakaoUser)

--- backend/src/main/java/com/oomool/api/domain/auth/service/OAuthService.java
+++ backend/src/main/java/com/oomool/api/domain/auth/service/OAuthService.java
@@ -76,4 +76,45 @@ public class OAuthService {
 
         return accessToken;
     }
+
+    public void createKakaoUser(String token) throws Exception {
+
+        String reqUrl = "https://kapi.kakao.com/v2/user/me";
+
+        //access_token을 이용하여 사용자 정보 조회
+        try {
+            URL url = new URL(reqUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Authorization", "Bearer " + token); //전송할 header 작성, access_token전송
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonObject parser = new JsonObject();
+            JsonElement element = JsonParser.parseString(result);
+
+            System.out.println("element : " + element);
+
+
+            br.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
