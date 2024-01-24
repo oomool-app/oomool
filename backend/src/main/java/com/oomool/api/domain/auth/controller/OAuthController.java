7d65981671935424dd8c88982c8f5d752f536361
김성수 <zxzxzlxlzlxl@naver.com>
2024-01-24 17:29:26 +0900

    ✨ feat: authorization_code를 발급받고 access_token 및 refresh token 발급 받기
    authorization_code는 클라이언트에서 따로 API를 요청한 뒤에 서버로 보내주면 서버에서 access token을 발급해준다.
    지금은 accesss token을 잘 불러오는지 테스트하기 위해 임의로 code를 받아오는 controller를 생성한 것이다.

---
+++ backend/src/main/java/com/oomool/api/domain/auth/controller/OAuthController.java
@@ -0,0 +1,30 @@
+package com.oomool.api.domain.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oomool.api.domain.auth.service.OAuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/oauth")
+public class OAuthController {
+
+    private final OAuthService oAuthService;
+
+    /**
+     * 카카오 callback
+     * [GET] /oauth/kakao/callback
+     */
+    @GetMapping("/kakao")
+    @ResponseBody
+    public void kakaoCallback(@RequestParam String code) {
+        System.out.println(code);
+        System.out.println("access token : " + oAuthService.getKakaoAccessToken(code));
+    }
+}
