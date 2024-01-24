67ea891b0d533d4b2f5a3754134ad6163c57d7f7
김성수 <zxzxzlxlzlxl@naver.com>
2024-01-25 00:57:23 +0900

    ✨ feat: 소셜 로그인 시 user 정보 및 userSocail 정보 DB에 저장
    user 정보와 social 정보(provder, provder_id)를 저장하기 위한 DTO와 UserService 로직 수정

--- backend/src/main/java/com/oomool/api/domain/auth/controller/OAuthController.java
+++ backend/src/main/java/com/oomool/api/domain/auth/controller/OAuthController.java
@@ -1,5 +1,7 @@
 package com.oomool.api.domain.auth.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,15 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oomool.api.domain.auth.service.OAuthService;
+import com.oomool.api.domain.user.dto.UserSocialDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/oauth")
+@Log4j2
 public class OAuthController {
 
     private final OAuthService oAuthService;
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     /**
      * 카카오 callback
@@ -25,9 +32,10 @@ public class OAuthController {
     @ResponseBody
     public void kakaoCallback(@RequestParam String code) {
         String accessToken = oAuthService.getKakaoAccessToken(code);
+        UserSocialDto userSocialDto = new UserSocialDto();
 
         try {
-            oAuthService.createKakaoUser(accessToken);
+            userSocialDto = oAuthService.createKakaoUser(accessToken);
         } catch (Exception e) {
             e.printStackTrace();
         }
