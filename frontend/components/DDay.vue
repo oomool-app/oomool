ec90ca1c555fe68b0ef770470d9d43c410b5d8ac
jep1995 <jep1995@naver.com>
2024-01-24 15:34:03 +0900

    🔨 chore: Date type 수정

--- frontend/components/DDay.vue
+++ frontend/components/DDay.vue
@@ -2,18 +2,16 @@
   <div>D-{{ dDay }}</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const dDay = ref(0);
 
 // Date 객체 생성
-const today = new Date();
+const today: any = new Date();
 // 종료일 가정
-const endDate = new Date(2024, 0, 26);
+const endDate: any = new Date(2024, 0, 26);
 // D-day 계산을 위해 24시간 더하기
 endDate.setHours(endDate.getHours() + 24);
 // D-day 계산
-const dayDiff = endDate - today;
+const dayDiff: number = endDate - today;
 dDay.value = Math.floor(dayDiff / (1000 * 60 * 60 * 24));
 </script>
-
-<style scoped></style>
