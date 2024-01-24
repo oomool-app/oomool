c58a6b7e1d83bc9c52ee7729de3f6eb361e35517
김성수 <zxzxzlxlzlxl@naver.com>
2024-01-24 12:30:54 +0900

    ✨ feat: swagger 환경 설정 및 실행 UI 확인

---
+++ backend/src/main/java/com/oomool/api/domain/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.oomool.api.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oomool.api.domain.user.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
