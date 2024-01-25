c3e9c38d41b422cda818e81618f276f318ab972f
bb05qudgus <bb05qudgus@naver.com>
2024-01-25 17:28:52 +0900

    ✨ feat: BackButton.vue 컴포넌트 작성

---
+++ frontend/components/BackButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button class="absolute" @click="back">
+      <svg
+        class="w-6 h-6"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15.75 19.5 8.25 12l7.5-7.5"
+        />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const back = (): void => {
+  window.history.go(-1);
+};
+</script>
