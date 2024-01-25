8249677fc46e06b8fa6c2fec4055bf106a486a7c
bb05qudgus <bb05qudgus@naver.com>
2024-01-25 17:34:38 +0900

    ✨ feat: MakeRoom 방 인원수 설정 페이지 작성

--- frontend/pages/makeroom/number.vue
+++ frontend/pages/makeroom/number.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-
+  <div class="flex-col m-4">
+    <BackButton></BackButton>
+    <h1 class="flex justify-center text-xl m-4 font-bold">방 만들기</h1>
+    <MakeRoomProgress :cur-page="'3'"></MakeRoomProgress>
+    <div class="font-bold mb-4">몇 명이 함께하나요?</div>
+    <div>최소 3명부터 최대 30명까지 선택할 수 있어요.</div>
+    <MakeRoomNumber></MakeRoomNumber>
+    <div>
+      <NuxtLink to="date"><Button>다음</Button></NuxtLink>
+    </div>
   </div>
 </template>
