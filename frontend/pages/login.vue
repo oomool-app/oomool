ea89bc4b1af09a789a62ddda72c2f58d186c4452
itsmo1031 <itsmo1031@gmail.com>
2024-01-26 16:20:14 +0900

    🚑 fix: change login page url redirect uri
    - must set OOMOOL_URL in your .env file after this commit

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -6,27 +6,24 @@
       <div>
         <a id="kakao-login-button" @click="handleKakaoLogin">
           <img
-            class ="pt-5 pl-10 pr-10"
+            class="pt-5 pl-10 pr-10"
             src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg"
             alt="카카오 로그인 버튼"
           />
         </a>
         <div>
-          {{route.query?.code}}
+          {{ route.query?.code }}
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
 const route = useRoute();
-const handleKakaoLogin = () : void=> {
+const handleKakaoLogin = (): void => {
   // 카카오 로그인 URL로 리다이렉트
-  const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?client_id=d63ba3628c4ea2cff1e3eb9f0955f47f&redirect_uri=http://localhost:3000/oauth/kakao&response_type=code`;
+  const kakaoLoginURL = `https://kauth.kakao.com/oauth/authorize?client_id=d63ba3628c4ea2cff1e3eb9f0955f47f&redirect_uri=${process.env.OOMOOL_SITE_URL}/oauth/kakao&response_type=code`;
   window.location.href = kakaoLoginURL;
 };
-
 </script>
