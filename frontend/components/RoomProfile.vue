b4cd45902dff3d4294e53deb43cabeda036e6de3
jep1995 <jep1995@naver.com>
2024-01-26 17:27:36 +0900

    ✨ feat: RoomProfile.vue component 작성

---
+++ frontend/components/RoomProfile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    id="profile"
+    class="w-24 h-32 border rounded-xl m-2 mt-6 pt-2 pb-2 flex flex-col justify-around items-center"
+  >
+    <div class="border rounded-full w-16 h-16">
+      <img src="../assets/images/정답 유령.png" alt="프로필 이미지" />
+    </div>
+    <div>{{ props.member.name }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface members {
+  name: string;
+}
+const props = defineProps<{
+  member: members;
+}>();
+</script>
+<style>
+#profile {
+  border: solid 2px #61339b;
+}
+</style>
