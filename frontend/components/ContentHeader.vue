abe7a1c2215469f9bbc2540a45ad3011b5205848
jep1995 <jep1995@naver.com>
2024-01-26 17:21:47 +0900

    ✨ feat: ContentHeader component 작성

---
+++ frontend/components/ContentHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header" class="header text-xl font-black">
+    {{ headerName }}
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  headerName: {
+    type: String,
+    default: '모임 이름',
+  },
+});
+</script>
+<style scope>
+#header {
+  color: #61339b;
+}
+</style>
