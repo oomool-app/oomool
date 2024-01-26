192b984e4c77c46f3d9b086a5315b93d43f29408
hinngreen <hinngreen@naver.com>
2024-01-26 17:13:02 +0900

    ✨ feat: 메인페이지 룸 카드 컴포넌트 작성

---
+++ frontend/components/RoomCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  <Card class="bg-gradient-to-t from-[#A8BDF9] to-primary border-0">
+      <CardHeader >
+        <div class="flex bg-white w-70 h-40 rounded-md justify-center items-center">내 친구가 유달리 멋있어 보였던 때는 언제인가요?</div>
+      </CardHeader>
+      <CardContent>
+          <div class="text-white text-xl font-bold">방 이름</div>
+      </CardContent>
+      <CardFooter >
+        <Button class="bg-yellow-300 text-black w-15 h-10">진행중</Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
