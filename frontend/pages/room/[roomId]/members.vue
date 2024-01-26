5e23dc07243b7f9e0b092a16870b143ef1161737
jep1995 <jep1995@naver.com>
2024-01-26 17:32:33 +0900

    🔨 chore: room/[roomId]/members.vue 폴더 구조 변경

---
+++ frontend/pages/room/[roomId]/members.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="mb-6 text-center">
+      <h1 class="text-2xl font-bold">전체 멤버 조회</h1>
+    </div>
+    <!--멤버 수대로 for문 처리-->
+
+    <div class="flow-root">
+      <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+        <li class="py-3 sm:py-4">
+          <div class="flex items-center space-x-4">
+            <div class="flex-shrink-0">
+              <img
+                class="w-8 h-8 rounded-full"
+                src="https://flowbite.com/docs/images/people/profile-picture-1.jpg"
+                alt="Neil image"
+              />
+            </div>
+            <div class="flex-1 min-w-0">
+              <p
+                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+              >
+                Neil Sims
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="py-3 sm:py-4">
+          <div class="flex items-center space-x-4">
+            <div class="flex-shrink-0">
+              <img
+                class="w-8 h-8 rounded-full"
+                src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                alt="Bonnie image"
+              />
+            </div>
+            <div class="flex-1 min-w-0">
+              <p
+                class="text-sm font-medium text-gray-900 truncate dark:text-white"
+              >
+                Bonnie Green
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
