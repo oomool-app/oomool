d27e6e855aa960904d656dce351ef6ba306e76a5
hinngreen <hinngreen@naver.com>
2024-01-29 17:06:18 +0900

    ✨ feat: 프로필 설정 페이지 랜덤 아바타 추가

--- frontend/pages/waitroom/[inviteCode]/updateProfile.vue
+++ frontend/pages/waitroom/[inviteCode]/updateProfile.vue
@@ -3,13 +3,15 @@
     <div class="mb-6 text-center">
       <h1 class="text-2xl pt-5 font-bold">프로필 설정하기</h1>
 
+      <!--프로필 이미지 미리보기-->
       <div class="flex pb-10 pt-10 justify-center items-start gap-2.5">
         <div class="relative flex h-32 w-32 px-0 py-2.5 justify-center rounded-full border-solid border-2 border-[#6D6D6D]" :style="{ backgroundColor: randomColor }">
-          <img src="/assets/images/정답 유령.png" alt="프로필 이미지" />
-          <RandomButton class="absolute right-0 bottom-0" @click="changeRandomButtonColor"></RandomButton>
+          <img :src ="randomAvatar" alt="프로필 아바타" />
+          <RandomButton class="absolute right-0 bottom-0" @click="changeRandom"></RandomButton>
         </div>
       </div>
 
+      <!--입력 창-->
       <div class="pl-5">
         <h1 class="pt-15 pb-1 text-xl font-bold flex justify-start">
           사용자 이름을 입력해주세요
@@ -28,33 +30,54 @@
           그룹 내 멤버들이 식별 가능한 닉네임을 입력해주세요
         </h1>
       </div>
-    </div>
 
-    <Button class="mx-auto" @click="back">프로필 수정하기</Button>
+    </div>
+    <!--설정하기 버튼-->
+    <Button class="mx-auto" @click="$router.push({ path: `/waitroom/${inviteCode}`})">프로필 설정하기</Button>
   </div>
 </template>
 
 <script setup lang="ts">
-const back = (): void => {
-  window.history.go(-1);
-};
+
+const route = useRoute();
 
 const colors: string[] = [
-  "#FF5733", "#33FF57", "#5733FF", "#FF33A1", "#33A1FF",
-  "#FFD700", "#4CAF50", "#2196F3", "#FFC107", "#795548",
-  "#E91E63", "#FF9800", "#9C27B0", "#00BCD4", "#607D8B"
+"#FFD8CC", "#CCFFD8", "#D8CCFF", "#FFD8F2", "#D8F2FF",
+  "#FFF3CC", "#CCE3FF", "#FFEAC4", "#C4B5A5",
+  "#F8C4D6", "#D8A5CC", "#B2E8EB", "#A4B5C3"
 ];
 
-const randomColor = ref(colors[0]);
+const avatarurls : string[] = [
+  "/img/곰유령.png", "/img/고양이유령.png", "/img/기본유령.png", "/img/기본유령2.png",
+  "/img/소라게유령.png", "/img/토끼유령.png", "/img/헤드셋유령.png"
+];
 
-const changeRandomButtonColor = (): void => {
+// 인덱스 랜덤 생성
+const randomColorIndex: number = Math.floor(Math.random() * colors.length);
+const randomAvatarIndex: number = Math.floor(Math.random() * avatarurls.length);
+
+// 최초로 주어지는 랜덤 프로필
+const randomColor = ref(colors[randomColorIndex]);
+const randomAvatar = ref(avatarurls[randomAvatarIndex]);
+
+// 랜덤 버튼 누르면 실행되는 함수들
+const changeRandom = (): void => {
   randomColor.value = getRandomColor();
+  randomAvatar.value = getRandomAvatar();
 };
 
+// 랜덤 색 반환
 function getRandomColor(): string {
-  const randomIndex: number = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
+  const randomColorIndex: number = Math.floor(Math.random() * colors.length);
+  return colors[randomColorIndex];
 }
 
+// 랜덤 아바타 반환
+function getRandomAvatar(): string {
+  const randomAvatarIndex: number = Math.floor(Math.random() * avatarurls.length);
+  return avatarurls[randomAvatarIndex];
+}
 
+// 링크에서 갖고 있는 초대코드 받아와서 다시 라우터링크에서 사용
+const inviteCode = route.params.inviteCode;
 </script>
