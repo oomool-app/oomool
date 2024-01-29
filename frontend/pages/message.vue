6588ef0735b5e397b2e9a43091a135bead305fb5
hinngreen <hinngreen@naver.com>
2024-01-29 17:04:29 +0900

    ✨ feat: 알림페이지   수정

--- frontend/pages/message.vue
+++ frontend/pages/message.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <div class="mb-6 bg-background">
-      <BackButton color="black" />
-      <h1 class="text-2xl font-bold pb-5">알림</h1>
+      <div class="flex justify-between pl-2">
+        <BackButton color="black" />
+        <h1 class="text-2xl font-bold pb-5 pr-10">알림</h1>
+      </div>
 
       <div class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
         <div v-for="message in messages" :key="message.messageid">
-          <MessageCard :messages="message"></MessageCard>
+          <MessageCard :messages="message" class="pt-3 pb-3"></MessageCard>
         </div>
       </div>
     </div>
@@ -18,30 +20,34 @@
 const messages = [
   {
     messageid: 1,
+    type: '생성',
     roomid: 1,
     roomtitle : '공통플젝 A809',
-    title: '7일차 질문이 생성되었습니다.',
-    createdtime : '2024-01-26',
-    readtime : '2024-01-27',
+    title: '오늘의 질문이 생성되었습니다.',
+    createdtime : '2024-01-29 10:08',
+    readtime : '2024-01-29 10:09',
   },
   {
     messageid: 2,
+    type: '답변 등록',
     roomid: 2,
     roomtitle : '서울8반 화이팅',
-    title: '2일차 질문이 생성되었습니다.',
-    createdtime : '2024-01-26',
+    title: '내 마니또가 답변을 등록했어요!',
+    createdtime : '2024-01-29 14:13',
     readtime : '2024-01-30',
   },
   {
     messageid: 3,
+    type: '종료',
     roomid: 3,
     roomtitle : '소문난 칠공주',
-    title: '3일차 질문이 생성되었습니다.',
+    title: '모든 세션이 종료되었어요. 결과를 확인하세요!',
     createdtime : '2024-01-22',
     readtime : '2024-01-23',
   },
   {
     messageid: 4,
+    type:'생성',
     roomid: 4,
     roomtitle : '싸피초등학교 16기',
     title: '6일차 질문이 생성되었습니다.',
