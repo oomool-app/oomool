e8dede464d52154b4e8973a5e8a65c1dc039a91f
bb05qudgus <bb05qudgus@naver.com>
2024-01-31 16:07:55 +0900

    ✨ feat: makeroom 인원수 설정 페이지 pinia 생성 props emit 설정

--- frontend/pages/makeroom/number.vue
+++ frontend/pages/makeroom/number.vue
@@ -5,9 +5,21 @@
     <MakeRoomProgress :cur-page="'3'"></MakeRoomProgress>
     <div class="font-bold mb-4">몇 명이 함께하나요?</div>
     <div>최소 3명부터 최대 30명까지 선택할 수 있어요.</div>
-    <MakeRoomNumber></MakeRoomNumber>
+    {{ store.number }}
+    <MakeRoomNumber
+      :people-number="store.number"
+      @update-number="handleUpdateNumber"
+    ></MakeRoomNumber>
     <div>
       <NuxtLink to="date"><Button>다음</Button></NuxtLink>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const store = useMakeRoomStore();
+
+function handleUpdateNumber(number: number): void {
+  store.number = number;
+}
+</script>
