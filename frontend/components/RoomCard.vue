56e5737a6bc3dd90d5cddc4836e314fa3da0748b
hinngreen <hinngreen@naver.com>
2024-01-31 16:50:06 +0900

    ✨ feat: 메인페이지 룸 카드 디자인 수정

--- frontend/components/RoomCard.vue
+++ frontend/components/RoomCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Card
-      class="w-100% h-auto bg-gradient-to-t from-[#A8BDF9] to-primary border-0 rounded-xl drop-shadow-xl transition-transform transform hover:filter hover:brightness-90"
+      class="w-100% h-auto bg-gradient-to-t from-[#A8BDF9] to-primary border-0 rounded-xl drop-shadow-xl transition-transform transform text-ellipsis overflow-hidden"
     >
       <!--진행중-->
       <div
@@ -10,7 +10,7 @@
       >
         <CardHeader>
           <div
-            class="flex bg-white font-bold px-5 py-10 w-70 h-40 shadow-inner rounded-xl justify-center items-center"
+            class="flex bg-white font-bold px-5 py-10 w-70 h-36 shadow-inner rounded-xl justify-center items-center"
           >
             {{ props.rooms.question }}
           </div>
@@ -33,7 +33,7 @@
           <PopoverTrigger as-child>
             <CardHeader as-child>
               <div
-                class="flex bg-white text-[#6D6D6D] font-bold px-5 py-10 w-70 h-40 shadow-inner rounded-xl justify-center items-center"
+                class="flex bg-white text-[#6D6D6D] font-bold px-5 py-10 w-70 h-36 shadow-inner rounded-xl justify-center items-center"
               >
                 아직 준비 중이에요!
               </div>
@@ -46,13 +46,19 @@
               </div>
             </CardContent>
             <CardFooter as-child>
-              <Button class="bg-[#F1D302] text-black w-15 h-10">시작대기</Button>
+              <Button class="bg-[#F1D302] text-black w-15 h-10"
+                >시작대기</Button
+              >
             </CardFooter>
           </PopoverTrigger>
           <PopoverContent>
             게임이 시작되지 않았습니다.
-            <div v-if="dayBeforeStart >= 1">{{ dayBeforeStart }}일 후에 시작됩니다.</div>
-            <div v-else-if="hourBeforeStart >= 1">{{ hourBeforeStart }}시간 후에 시작됩니다.</div>
+            <div v-if="dayBeforeStart >= 1">
+              {{ dayBeforeStart }}일 후에 시작됩니다.
+            </div>
+            <div v-else-if="hourBeforeStart >= 1">
+              {{ hourBeforeStart }}시간 후에 시작됩니다.
+            </div>
             <div v-else>{{ minBeforeStart }}분 후에 시작됩니다.</div>
           </PopoverContent>
         </Popover>
@@ -61,11 +67,11 @@
       <!--종료됨-->
       <div
         v-else-if="!endDateIsAfterToday"
-        @click="$router.push({ path: `final/${rooms.roomid}/result1` })"
+        @click="$router.push({ path: `guess/${rooms.roomid}` })"
       >
         <CardHeader>
           <div
-            class="flex bg-white text-[#6D6D6D] font-bold px-5 py-10 w-70 h-40 shadow-inner rounded-xl justify-center items-center"
+            class="flex bg-white text-[#6D6D6D] font-bold px-5 py-10 w-70 h-36 shadow-inner rounded-xl justify-center items-center"
           >
             종료된 방입니다.
           </div>
@@ -101,10 +107,12 @@ const props = defineProps<{
 
 const today: Date = new Date();
 
-const startDateIsBeforeToday = new Date(props.rooms.startdate as string) < today;
+const startDateIsBeforeToday =
+  new Date(props.rooms.startdate as string) < today;
 const endDateIsAfterToday = new Date(props.rooms.enddate as string) > today;
-const timeBeforeStart = new Date(props.rooms.startdate as string).getTime() - today.getTime();
-const dayBeforeStart =  Math.floor(timeBeforeStart/ (1000 * 60 * 60 * 24));
+const timeBeforeStart =
+  new Date(props.rooms.startdate as string).getTime() - today.getTime();
+const dayBeforeStart = Math.floor(timeBeforeStart / (1000 * 60 * 60 * 24));
 const minBeforeStart = Math.floor(timeBeforeStart / 1000 / 60);
 const hourBeforeStart = Math.floor(timeBeforeStart / (1000 * 60 * 60));
 </script>
