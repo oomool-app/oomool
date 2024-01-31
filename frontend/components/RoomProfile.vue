863ae426508d86dfc82a07d0ee7013e9c78482d3
jep1995 <jep1995@naver.com>
2024-01-31 17:28:51 +0900

    🔨 chore: 유령 파일명 수정

--- frontend/components/RoomProfile.vue
+++ frontend/components/RoomProfile.vue
@@ -4,7 +4,7 @@
     class="w-24 h-32 border rounded-xl m-2 mt-6 pt-2 pb-2 flex flex-col justify-around items-center"
   >
     <div class="border rounded-full w-16 h-16">
-      <img src="/img/정답 유령.png" alt="프로필 이미지" />
+      <img src="/img/정답유령.png" alt="프로필 이미지" />
     </div>
     <div>{{ props.member.name }}</div>
   </div>
