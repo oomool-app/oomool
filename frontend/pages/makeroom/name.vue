876bc1daf315a97d2773d68196b4ce7fdba1fcd1
bb05qudgus <bb05qudgus@naver.com>
2024-01-31 16:06:31 +0900

    ✨ feat: makeroom 이름 설정 페이지 pinia 생성 props emit 설정

--- frontend/pages/makeroom/name.vue
+++ frontend/pages/makeroom/name.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="flex-col m-4">
-    <BackButton color="black"></BackButton>
-    <h1 class="flex justify-center text-xl m-4 font-bold">방 만들기</h1>
+  <div class="flex-col h-screen">
+    <div class="grid grid-cols-3">
+      <div class="flex items-center">
+        <BackButton color="black"></BackButton>
+      </div>
+      <div class="flex justify-center items-center">
+        <h1 class="text-xl font-bold">방 만들기</h1>
+      </div>
+    </div>
     <MakeRoomProgress :cur-page="'1'"></MakeRoomProgress>
-    <div class="font-bold mb-4">우리 방의 이름을 정해주세요!</div>
+    <div class="font-bold">우리 방의 이름을 정해주세요!</div>
     <div>
       <input
+        id="userName"
+        v-model="store.name"
+        name="userName"
+        type="text"
         class="w-full border border-purple-800 rounded h-8"
-        placeholder="김병현"
+        :placeholder="userName"
+        autocomplete="off"
       />
     </div>
-    <div>
+    <div class="flex justify-center items-end grow">
       <NuxtLink to="type"><Button>다음</Button></NuxtLink>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const store = useMakeRoomStore();
+const userName = ref<string>('계정 이름');
+</script>
