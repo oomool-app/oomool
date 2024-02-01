fb77359acda0a86e976635fc93cd401df40ce96e
sejeong-park <tpwjd29@gmail.com>
2024-02-02 02:27:23 +0900

    ♻️ refactor: modify gameRoomService to query SettingOption and PlayerList
    - 문답방의 설정값 조회 로직 추가
    - gameRoomService에서 Entity 활용해 객체로 저장될 수 있게 변경

--- backend/src/main/java/com/oomool/api/domain/room/service/GameRoomService.java
+++ backend/src/main/java/com/oomool/api/domain/room/service/GameRoomService.java
@@ -1,13 +1,10 @@
 package com.oomool.api.domain.room.service;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.oomool.api.domain.player.dto.PlayerDto;
-import com.oomool.api.domain.player.service.PlayerService;
 import com.oomool.api.domain.room.dto.SettingOptionDto;
 import com.oomool.api.domain.room.entity.GameRoom;
 import com.oomool.api.domain.room.repository.GameRoomRepository;
@@ -25,7 +22,6 @@ public class GameRoomService {
     private final GameRoomRepository gameRoomRepository;
     private final GameRoomMapper gameRoomMapper;
     private final TempRoomRedisService tempRoomRedisService;
-    private final PlayerService playerService;
 
     /**
      * 문답방 생성
@@ -47,19 +43,28 @@ public class GameRoomService {
      * */
     public Map<String, Object> getGameRoom(String roomUid) {
 
-        // 플레이어 Dto List 조회
-        List<PlayerDto> playerDtoList = playerService.getPlayerDtoList(roomUid);
         // 문답방 setting 데이터 조회
         GameRoom gameRoom = gameRoomRepository.findByRoomUid(roomUid)
             .orElseThrow(() -> new EntityNotFoundException("room UID가 존재하지 않습니다."));
 
-        SettingOptionDto settingOptionDto = gameRoomMapper.convertSettingOptionDto(gameRoom, playerDtoList.size());
+        SettingOptionDto settingOptionDto = gameRoomMapper.convertSettingOptionDto(gameRoom);
 
         return Map.of(
             "room_uid", roomUid,
             "create_at", gameRoom.getCreateAt(),
             "setting", settingOptionDto,
-            "players", playerDtoList);
+            "players", gameRoom.getPlayers());
+    }
+
+    /**
+     * 문답방 설정값 조회
+     * */
+    public SettingOptionDto getSeeingOptionDto(String roomUid) {
+
+        GameRoom gameRoom = gameRoomRepository.findByRoomUid(roomUid)
+            .orElseThrow(() -> new EntityNotFoundException("room UID가 존재하지 않습니다."));
+
+        return gameRoomMapper.convertSettingOptionDto(gameRoom);
     }
 
 }
