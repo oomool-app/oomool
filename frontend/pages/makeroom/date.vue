e8dc5dd65e656b84f3874c19559cc6d2c2366d9b
bb05qudgus <bb05qudgus@naver.com>
2024-02-01 17:18:54 +0900

    ✨ feat: api를 통한 대기방 생성

--- frontend/pages/makeroom/date.vue
+++ frontend/pages/makeroom/date.vue
@@ -5,17 +5,65 @@
     <MakeRoomProgress :cur-page="'4'"></MakeRoomProgress>
     <div class="font-bold mb-4">언제부터 언제까지 진행되나요?</div>
     <div>최대 14일까지 설정할 수 있어요.</div>
+    <div id="check" class="text-red-600"></div>
     <div class="flex justify-center items-center">
       <Calendar v-model.range="store.range" type="range"> </Calendar>
     </div>
-    {{ store.range.start }}
-    {{ store.range.end }}
     <div>
-      <NuxtLink to="/waitroom/1"><Button>만들기</Button></NuxtLink>
+      <NuxtLink to="/waitroom/1"
+        ><Button @click="check">만들기</Button></NuxtLink
+      >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const store = useMakeRoomStore();
+const userInfo: any = localStorage.getItem('user');
+const check = async (event: any): Promise<void> => {
+  const element = document.getElementById('check');
+  if (element == null) {
+    return;
+  }
+  element.innerText = '';
+  const period = store.diffDate(
+    store.formatDate(store.range.start),
+    store.formatDate(store.range.end),
+  );
+  if (
+    store.diffDate(
+      store.formatDate(new Date()),
+      store.formatDate(store.range.start),
+    ) <= 0
+  ) {
+    element.innerText = '시작일을 다시 설정해주세요!';
+    event.preventDefault();
+    return;
+  } else if (period > 14 || period < 3) {
+    element.innerText =
+      '3일 이상 14일이하로 설정해주세요! 현재 : (' + period + '일)';
+    event.preventDefault();
+    return;
+  }
+
+  const { data } = await useFetch('https://api-dev.oomool.site/temp', {
+    method: 'POST',
+    body: JSON.stringify({
+      setting: {
+        title: store.name,
+        start_date: store.formatDate(store.range.start),
+        end_date: store.formatDate(store.range.end),
+        question_type: 'BF',
+        max_member: store.number,
+      },
+      master: {
+        user_id: userInfo.user_id,
+        user_email: userInfo.user_email,
+        player_nickname: userInfo.user_nickname,
+        player_background_color: userInfo.player_background_color,
+        player_avatar_url: userInfo.player_avatar_url,
+      },
+    }),
+  });
+};
 </script>
