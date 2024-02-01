890b5bf45a77a7edc5f3a6eb44949bc7b6df021a
sejeong-park <tpwjd29@gmail.com>
2024-02-02 02:34:59 +0900

    🔧 chore: add Repository Annotation

--- backend/src/main/java/com/oomool/api/domain/question/repository/RoomQuestionReposiotry.java
+++ backend/src/main/java/com/oomool/api/domain/question/repository/RoomQuestionReposiotry.java
@@ -1,8 +1,10 @@
 package com.oomool.api.domain.question.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.oomool.api.domain.question.entity.RoomQuestion;
 
+@Repository
 public interface RoomQuestionReposiotry extends JpaRepository<RoomQuestion, Integer> {
 }
