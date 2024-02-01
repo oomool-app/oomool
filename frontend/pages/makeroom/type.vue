0fd832e39b3153a61c8e09e445f4e567a277e9cf
bb05qudgus <bb05qudgus@naver.com>
2024-02-01 17:20:14 +0900

    ✨ feat: 방 타입 생성 유효성 검사 추가

--- frontend/pages/makeroom/type.vue
+++ frontend/pages/makeroom/type.vue
@@ -5,12 +5,13 @@
     <MakeRoomProgress :cur-page="'2'"></MakeRoomProgress>
     <div class="font-bold mb-4">우리들, 어떤 사이예요?</div>
     <div>매일 제공되는 질문의 유형이 결정돼요!</div>
+    <div v-if="typeCheck" class="text-red-600">질문의 유형을 정해주세요!</div>
     <MakeRoomTypeButton
       :game-type="store.type"
       @update-type="handleUpdateType"
     ></MakeRoomTypeButton>
     <div>
-      <NuxtLink to="number"><Button>다음</Button></NuxtLink>
+      <NuxtLink to="number"><Button @click="check">다음</Button></NuxtLink>
     </div>
   </div>
 </template>
@@ -21,4 +22,11 @@ const store = useMakeRoomStore();
 function handleUpdateType(title: string): void {
   store.type = title;
 }
+const typeCheck = ref<boolean>(false);
+const check = (e: any): void => {
+  if (store.type === '') {
+    typeCheck.value = true;
+    e.preventDefault();
+  }
+};
 </script>
