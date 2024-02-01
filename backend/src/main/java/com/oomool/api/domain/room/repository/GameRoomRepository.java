3bc293c86e7df75938c0543ab930b03bb68b230f
박세정 <mblaqsj1015@naver.com>
2024-02-01 17:32:16 +0900

    ✨ feat: DailyQuestion Controller 개발

--- backend/src/main/java/com/oomool/api/domain/room/repository/GameRoomRepository.java
+++ backend/src/main/java/com/oomool/api/domain/room/repository/GameRoomRepository.java
@@ -3,8 +3,10 @@ package com.oomool.api.domain.room.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.oomool.api.domain.question.dto.QuestionCreationDto;
 import com.oomool.api.domain.room.entity.GameRoom;
 
 @Repository
@@ -12,4 +14,8 @@ public interface GameRoomRepository extends JpaRepository<GameRoom, Integer> {
 
     // roomUid에 대한 조회 메서드
     Optional<GameRoom> findByRoomUid(String roomUid);
+
+    // Question 생성을 위한 조건 조회
+    @Query("SELECT g.startDate, g.endDate, g.questionType FROM GameRoom g WHERE g.roomUid=:roomUid")
+    QuestionCreationDto findQuestionCreationByRoomUid(String roomUid);
 }
