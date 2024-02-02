897c5bf2326e8605bc515ea61dce7cd12f074e1f
itsmo1031 <itsmo1031@gmail.com>
2024-02-02 13:07:30 +0900

    ♻️ refactor: remove redundant Generic declaration

--- backend/src/main/java/com/oomool/api/global/util/ResponseHandler.java
+++ backend/src/main/java/com/oomool/api/global/util/ResponseHandler.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 
 public class ResponseHandler {
     public static ResponseEntity<Object> generateResponse(HttpStatus status, Object response) {
-        Map<String, Object> map = new HashMap<String, Object>();
+        Map<String, Object> map = new HashMap<>();
         map.put("status", status);
         map.put("data", response);
         return new ResponseEntity<Object>(map, status);
