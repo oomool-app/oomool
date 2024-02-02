0087359ff3d0146c3d569e422fd6fc5303e7b93a
itsmo1031 <itsmo1031@gmail.com>
2024-02-02 13:10:55 +0900

    ✨ feat: add response of controller endpoint

--- backend/src/main/java/com/oomool/api/domain/notification/controller/PushNotificationController.java
+++ backend/src/main/java/com/oomool/api/domain/notification/controller/PushNotificationController.java
@@ -1,5 +1,9 @@
 package com.oomool.api.domain.notification.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.oomool.api.domain.notification.dto.PushNotificationTokenDto;
 import com.oomool.api.domain.notification.service.PushNotificationService;
+import com.oomool.api.global.util.ResponseHandler;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,21 +31,27 @@ public class PushNotificationController {
     // 토큰 저장에 대한 API 생성
     @PostMapping("/token")
     @Operation(summary = "푸시 알림 토큰 저장 기능", description = "푸시 알림 토큰을 저장합니다.")
-    public void saveToken(@RequestBody PushNotificationTokenDto pushNotificationTokenDto) {
+    public ResponseEntity<?> saveToken(@RequestBody PushNotificationTokenDto pushNotificationTokenDto) {
         pushNotificationService.saveToken(pushNotificationTokenDto.userId(), pushNotificationTokenDto.token());
+
+        return ResponseHandler.generateResponse(HttpStatus.CREATED, "푸시 알림 토큰이 저장되었습니다.");
     }
 
     // 토큰 삭제에 대한 API 생성
     @DeleteMapping("/token")
     @Operation(summary = "푸시 알림 토큰 삭제 기능", description = "푸시 알림 토큰을 삭제합니다.")
-    public void removeToken(@RequestBody PushNotificationTokenDto pushNotificationTokenDto) {
+    public ResponseEntity<?> removeToken(@RequestBody PushNotificationTokenDto pushNotificationTokenDto) {
         pushNotificationService.removeToken(pushNotificationTokenDto.userId(), pushNotificationTokenDto.token());
+
+        return ResponseHandler.generateResponse(HttpStatus.OK, "푸시 알림 토큰이 삭제되었습니다.");
     }
 
     // 유저의 모든 토큰 조회에 대한 API 생성
     @GetMapping("/token/{userId}")
     @Operation(summary = "푸시 알림 토큰 조회 기능", description = "유저의 모든 푸시 알림 토큰을 조회합니다.")
-    public void getAllTokensByUser(@PathVariable("userId") int userId) {
-        pushNotificationService.getAllTokensByUser(userId);
+    public ResponseEntity<?> getAllTokensByUser(@PathVariable("userId") int userId) {
+
+        return ResponseHandler.generateResponse(HttpStatus.OK,
+            Map.of("tokens", pushNotificationService.getAllTokensByUser(userId)));
     }
 }
