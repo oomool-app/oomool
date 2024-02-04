4aef180274aa4ff207b5f08825049a2485ebeed0
sejeong-park <tpwjd29@gmail.com>
2024-02-04 17:20:07 +0900

    ♻️ refactor: refactor Question Service Layer

---
+++ backend/src/main/java/com/oomool/api/domain/question/service/RoomQuestionServiceImpl.java
@@ -0,0 +1,85 @@
+package com.oomool.api.domain.question.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oomool.api.domain.question.dto.DailyQuestionDto;
+import com.oomool.api.domain.question.entity.Question;
+import com.oomool.api.domain.question.entity.RoomQuestion;
+import com.oomool.api.domain.question.repository.QuestionRepository;
+import com.oomool.api.domain.question.repository.RoomQuestionReposiotry;
+import com.oomool.api.domain.question.util.RoomQuestionUtil;
+import com.oomool.api.domain.room.entity.GameRoom;
+import com.oomool.api.global.util.CustomDateUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RoomQuestionServiceImpl implements RoomQuestionService {
+
+    private final RoomQuestionReposiotry roomQuestionReposiotry;
+    private final QuestionRepository questionRepository;
+
+    // 문답방의 질문 생성기
+    private final RoomQuestionUtil roomQuestionUtil;
+
+    @Override
+    public void publishRoomQuestionList(GameRoom gameRoom) {
+
+        // 질문 생성에 필요한 조건을 구한다.
+        // 1. 몇일 동안 진행될 것인지
+        // 2. 질문의 깊이는 어느정도인지
+        int dateInterval = CustomDateUtil.getDateInterval(gameRoom.getStartDate(),
+            gameRoom.getEndDate());
+        int questionTypeDepth = roomQuestionUtil.getQuestionTypeDepth(gameRoom.getQuestionType());
+
+        // 기간과 깊이를 고려하여, 레벨 별로 진행될 수량을 추출한다.
+        Map<Integer, Integer> levelDistribution = roomQuestionUtil.calculateLevelDistribution(dateInterval,
+            questionTypeDepth);
+
+        // 뽑은 Question 목록을 레벨별 수량에 맞춰 추출한다.
+        List<Question> targetQuestionList = extractTargetQuestionList(questionTypeDepth, levelDistribution);
+
+        // 추출한 targetQuestionList를 RoomQuestion Entity로 변환한다.
+        List<RoomQuestion> roomQuestionList = IntStream.range(0, targetQuestionList.size())
+            .mapToObj(
+                index -> roomQuestionUtil.convertToRoomQuestion(targetQuestionList.get(index), gameRoom, index + 1))
+            .collect(Collectors.toList());
+
+        roomQuestionReposiotry.saveAll(roomQuestionList); // roomQuestion 목록 저장
+    }
+
+    @Override
+    public List<Question> extractTargetQuestionList(int questionTypeDepth,
+        Map<Integer, Integer> levelDistribution) {
+
+        // 미리 선정해둔 Question 목록에서 레벨과 질문 수량에 맞게 DB에서 가져온다.
+        // - Question 목록에서 Level 수량을 "랜덤"하게 셔플하여 가져온다.
+
+        List<Question> targetQuestionList = new ArrayList<>();
+        for (int level = 1; level <= questionTypeDepth; level++) {
+            // Level 별 Question 목록을 level 별로 가져온다.
+            List<Question> targetQuestionListAtLevel = questionRepository.findByLevel(level);
+            // Question 목록을 분배 수량 만큼 <랜덤>으로 가져온다.
+            targetQuestionList.addAll(
+                roomQuestionUtil.getRandomQuestion(
+                    targetQuestionListAtLevel,
+                    levelDistribution.get(level)
+                ));
+        }
+        return targetQuestionList;
+    }
+
+    @Override
+    public DailyQuestionDto getDailyQuestion(String roomUid) {
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/oomool/api/domain/question/util/RoomQuestionUtil.java
@@ -0,0 +1,74 @@
+package com.oomool.api.domain.question.util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.oomool.api.domain.question.entity.Question;
+import com.oomool.api.domain.question.entity.QuestionType;
+import com.oomool.api.domain.question.entity.RoomQuestion;
+import com.oomool.api.domain.room.entity.GameRoom;
+
+/**
+ * 전체 Question 목록에서 문답방에서 사용할 RoomQuestion 목록을 생성하는데, 사용하는 Util
+ * */
+
+@Component
+public class RoomQuestionUtil {
+
+    /**
+     * QuestionType의 Depth 정도 구한다. -> 3 or 5
+     *
+     * @param questionType 질문 유형
+     * */
+    public int getQuestionTypeDepth(QuestionType questionType) {
+        return (questionType == QuestionType.AW) ? 3 : (questionType == QuestionType.BF) ? 5 : -1;
+    }
+
+    /**
+     * level 별 질문 분배
+     *
+     * @param dateInterval 날짜 기간
+     * @param questionTypeDepth 질문 깊이
+     * */
+    public Map<Integer, Integer> calculateLevelDistribution(int dateInterval, int questionTypeDepth) {
+        Map<Integer, Integer> levelDistribution = new HashMap<Integer, Integer>();
+        int baseCount = dateInterval / questionTypeDepth;                // 기본적으로 할당할 수량
+        for (int level = 1; level <= questionTypeDepth; level++) {
+            int extra = level <= dateInterval % questionTypeDepth ? 1 : 0;
+            levelDistribution.put(level, baseCount + extra);
+        }
+        return levelDistribution;
+    }
+
+    /**
+     * Question Entity를 RoomQuesetion Entity로 매핑한다.
+     *
+     * @param question 질문
+     * @param gameRoom 문답방
+     * @param sequence 순서
+     * */
+    public RoomQuestion convertToRoomQuestion(Question question, GameRoom gameRoom, int sequence) {
+        return RoomQuestion.builder()
+            .room(gameRoom)
+            .question(question)
+            .sequence(sequence)
+            .build();
+    }
+
+    /**
+     * level 별 질문 랜덤하게 섞기
+     *
+     * @param targetQuestionList 변환할 질문 목록
+     * @param count level 별로 분배된 수량
+     * */
+    public List<Question> getRandomQuestion(List<Question> targetQuestionList, int count) {
+        Collections.shuffle(targetQuestionList);
+        return new ArrayList<>(targetQuestionList.subList(0,
+            Math.min(count, targetQuestionList.size()))); // count가 questionList보다 작을 리 없게 설계되어야 한다. -> Question 여유있게.
+    }
+}
