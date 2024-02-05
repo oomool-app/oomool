7699c59ec54c9a3dee0809fb276a2221f39a1cd6
itsmo1031 <itsmo1031@gmail.com>
2024-02-05 16:51:08 +0900

    🚑️ fix: Feed 관련 conflict resolve

--- backend/src/main/java/com/oomool/api/domain/feed/dto/FeedAnswerDto.java
+++ backend/src/main/java/com/oomool/api/domain/feed/dto/FeedAnswerDto.java
@@ -2,8 +2,10 @@ package com.oomool.api.domain.feed.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -13,18 +15,13 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FeedAnswerDto {
     private int feedId; // 답변을 등록한 해당 피드 id
     private String content; // 답변 내용
     private List<FeedImageDto> feedImageDtoList; // 이미지 파일
     private int authorId; // 작성자 아이디
-
-    @Builder
-    public FeedAnswerDto(int feedId, String content, List<FeedImageDto> feedImageDtoList, int authorId) {
-        this.feedId = feedId;
-        this.content = content;
-        this.feedImageDtoList = feedImageDtoList;
-        this.authorId = authorId;
-    }
 }
 
