cdbb35eb17ba5c23e3ee23a94b42e5a57e143812
bb05qudgus <bb05qudgus@naver.com>
2024-02-06 18:05:05 +0900

    ✨ feat: waitroom api repository interface 추가

---
+++ frontend/repository/modules/interface/waitroom.interface.ts
@@ -0,0 +1,79 @@
+export interface IGetMakeRoomResponse {
+  data: {
+    created_at: string;
+    invite_code: string;
+    master_id: string;
+    setting: {
+      title: string;
+      start_date: string;
+      end_date: string;
+      question_type: string;
+      max_member: number;
+    };
+  };
+  status: string;
+}
+
+export interface IMakeRoomInput {
+  setting: {
+    title: string;
+    start_date: string;
+    end_date: string;
+    question_type: string;
+    max_member: number;
+  };
+  master: {
+    id: number;
+    email: string;
+    username: string;
+  };
+}
+
+export interface IJoinWaitRoomInput {
+  user_id: number;
+  user_email: string;
+  player_nickname: string;
+  player_background_color: string;
+  player_avatar_url: string;
+}
+
+export interface IGetJoinWaitRoomResponse {
+  data: {
+    players: [
+      {
+        user_id: number;
+        user_email: string;
+        player_nickname: string;
+        player_background_color: string;
+        player_avatar_url: string;
+      },
+    ];
+  };
+  status: string;
+}
+
+export interface IGetWaitRoomResponse {
+  data: {
+    invite_code: string;
+    master_id: number;
+    setting: {
+      title: string;
+      start_date: string;
+      end_date: string;
+      question_type: string;
+      max_member: number;
+    };
+    players: [
+      {
+        user_id: number;
+        user_email: string;
+        player_nickname: string;
+        player_background_color: string;
+        player_avatar_url: string;
+      },
+    ];
+    ban_list: string;
+    created_at: string;
+  };
+  status: string;
+}
