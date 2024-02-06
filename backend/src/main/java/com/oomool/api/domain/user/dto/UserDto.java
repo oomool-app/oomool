f89346f863bff054314274fe74987b8612f4487c
김성수 <zxzxzlxlzlxl@naver.com>
2024-02-06 09:25:20 +0900

    ♻️ refactor: deleted @Setter userDto

--- backend/src/main/java/com/oomool/api/domain/user/dto/UserDto.java
+++ backend/src/main/java/com/oomool/api/domain/user/dto/UserDto.java
@@ -4,11 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
