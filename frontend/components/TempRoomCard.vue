1a609f08361496979c2e63c65c4cc4822a5fd71c
hinngreen <hinngreen@naver.com>
2024-02-06 12:53:57 +0900

    ✨ feat: 대기방 목록 조회 카드 컴포넌트 생성

---
+++ frontend/components/TempRoomCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <Card
+      class="w-100% h-auto bg-gradient-to-t from-[#A8BDF9] to-primary border-0 rounded-xl drop-shadow-xl transition-transform transform text-ellipsis overflow-hidden"
+    >
+      <div
+        class="flex flex-col"
+        @click="$router.push({ path: `waitroom/${temprooms.invite_code}` })"
+      >
+        <CardHeader class="px-3.5 pt-3.5 pb-4">
+          <div
+            class="flex bg-white text-[#6D6D6D] font-bold px-3 py-10 w-70 h-24 shadow-inner rounded-xl justify-center items-center"
+          >
+            대기 방입니다.
+          </div>
+        </CardHeader>
+        <CardContent class="px-3 pt-0">
+          <div class="text-white text-xl not-italic h-3">
+            {{ props.temprooms.setting.title }}
+          </div>
+        </CardContent>
+        <CardFooter class="px-3 pb-3 pl-4">
+          <Button
+            class="bg-[#6D6D6D] text-black w-14 h-6 text-center align-middle text-xs rounded-full"
+            >대기방</Button
+          >
+        </CardFooter>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface TempRoom {
+  invite_code: string;
+  setting: {
+    title: string;
+    start_date: string;
+    end_date: string;
+    question_type: string;
+    max_member: number;
+  };
+  master_id: number;
+}
+
+const props = defineProps<{
+  temprooms: TempRoom;
+}>();
+</script>
