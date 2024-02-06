a82e4bd1f5b568739b3040425f45425b7a79ec0d
sejeong-park <tpwjd29@gmail.com>
2024-02-07 02:39:58 +0900

    🚧 feat: develop of Format a Custom Exception
    - 사용자 정의 예외 처리 Class 추가

---
+++ backend/src/main/java/com/oomool/api/global/exception/StatusCode.java
@@ -0,0 +1,33 @@
+package com.oomool.api.global.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum StatusCode {
+
+    /**
+     * Common
+     * */
+
+    // 4XX
+    FORBIDDEN(403, "Forbidden"),
+
+    /**
+     * 대기방
+     * */
+    INVALID_INVITE_CODE(404, "유효하지 않은 초대 코드입니다."),
+    NOT_MASTER_AUTH_TEMPROOM(404, "방장의 권한이 없습니다."),
+    DUPLICATION_INVITE_CODE(404, "이미 참여하고 있는 대기방 입니다."),
+    NOT_COMPLETE_SETTING_OPTION(400, "설정 옵션이 지정되지 않았습니다."),
+
+    /**
+     * 문답방
+     * */
+    INVILD_OOMOOL_ROOM_UUID(404, "유효하지 않은 방 UUID 입니다."),
+    CANNOT_START_OOMOOL_ROOM(400, "방을 생성할 수 없습니다.");
+
+    private final int statusCode;
+    private final String message;
+}
