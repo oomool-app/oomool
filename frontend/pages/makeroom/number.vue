ee402a437bdae8f3dd7a38f49577c1b0fdb69c1f
bb05qudgus <bb05qudgus@naver.com>
2024-02-06 23:28:28 +0900

    💄 style: 방 인원수 설정 페이지 css 변경

--- frontend/pages/makeroom/number.vue
+++ frontend/pages/makeroom/number.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="box flex-col p-6">
-    <div class="flex justify-between">
-      <BackButton color="black"></BackButton>
-      <h1 class="text-xl font-bold">방 만들기</h1>
-      <div class="mr-6"></div>
+  <div class="box grid grid-rows-[6rem,2rem,6rem,5rem] h-screen p-6">
+    <div class="grid grid-cols-3">
+      <div class="flex items-center">
+        <BackButton color="black"></BackButton>
+      </div>
+      <div class="flex items-center justify-center text-2xl font-extrabold">
+        방 만들기
+      </div>
     </div>
-    <MakeRoomProgress :cur-page="'3'"></MakeRoomProgress>
-    <div class="font-bold mb-4">몇 명이 함께하나요?</div>
-    <div>최소 3명부터 최대 30명까지 선택할 수 있어요.</div>
-    {{ store.number }}
-    <MakeRoomNumber
-      :people-number="store.number"
-      @update-number="handleUpdateNumber"
-    ></MakeRoomNumber>
     <div>
-      <NuxtLink to="date"><Button>다음</Button></NuxtLink>
+      <MakeRoomProgress :cur-page="'3'"></MakeRoomProgress>
+    </div>
+    <div>
+      <div class="font-bold text-xl pt-2">몇 명이 함께하나요?</div>
+      <div class="text-sm">최소 3명부터 최대 30명까지 선택할 수 있어요.</div>
+    </div>
+    <div class="flex flex-col">
+      <div class="flex justify-center items-center text-2xl font-bold mb-3">
+        {{ store.number }} 명
+      </div>
+      <MakeRoomNumber
+        :people-number="store.number"
+        @update-number="handleUpdateNumber"
+      ></MakeRoomNumber>
+    </div>
+    <div class="flex justify-center items-end pb-6">
+      <NuxtLink class="w-full rounded-full" to="date">
+        <Button class="w-full rounded-full text-lg">다음</Button>
+      </NuxtLink>
     </div>
   </div>
 </template>
