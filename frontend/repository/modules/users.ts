89d55d6c706ea97c4e192b0585d985cff12962a1
hinngreen <hinngreen@naver.com>
2024-02-06 12:55:02 +0900

    ✨ feat: 대기방 목록 조회 api repository 추가

--- frontend/repository/modules/users.ts
+++ frontend/repository/modules/users.ts
@@ -2,6 +2,8 @@ import HttpFactory from '../factory';
 import {
   type IGetRoomListInput,
   type IGetRoomListResponse,
+  type IGetTempRoomListInput,
+  type IGetTempRoomListResponse,
 } from './interface/users.interface';
 
 class UsersModule extends HttpFactory {
@@ -12,6 +14,16 @@ class UsersModule extends HttpFactory {
     return await this.getCall<IGetRoomListResponse>(
       'GET',
       `${this.RESOURCE}/${userId.userId}/games`,
+    );
+  }
+
+  // 유저아이디로 대기방 목록 가져오기
+  async getTempRoomList(
+    userId: IGetTempRoomListInput,
+  ): Promise<IGetTempRoomListResponse> {
+    return await this.getCall<IGetTempRoomListResponse>(
+      'GET',
+      `${this.RESOURCE}/${userId.userId}/temps`,
     );
   }
 }
--- frontend/stores/userStore.ts
+++ frontend/stores/userStore.ts
@@ -12,21 +12,21 @@ export const useUserStore = defineStore({
     user: null as null | User,
   }),
   actions: {
-    // 사용자 정보를 설정하고 세션 스토리지에 저장
+    // 사용자 정보를 설정하고 로컬 스토리지에 저장
     setUser(user: User): void {
       this.user = user;
-      if (typeof sessionStorage !== 'undefined') {
-        sessionStorage.setItem('user', JSON.stringify(user));
+      if (typeof localStorage !== 'undefined') {
+        localStorage.setItem('user', JSON.stringify(user));
       }
     },
-    // 세션 스토리지에서 사용자 정보 가져오기
+    // 로컬 스토리지에서 사용자 정보 가져오기
     getStoredUser(): User | null {
-      const storedUser = sessionStorage.getItem('user');
+      const storedUser = localStorage.getItem('user');
       if (storedUser !== null) {
-        // 세션 스토리지에 사용자 정보가 있으면 파싱하여 반환
+        //  로컬 스토리지에 사용자 정보가 있으면 파싱하여 반환
         return JSON.parse(storedUser);
       } else {
-        // 세션 스토리지에 사용자 정보가 없으면 null 반환
+        // 로컬 스토리지에 사용자 정보가 없으면 null 반환
         return null;
       }
     },
