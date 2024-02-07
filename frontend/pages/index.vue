38c712ca6b8a1a5bbe210ae6713bd85a57324c3a
hinngreen <hinngreen@naver.com>
2024-02-07 17:00:13 +0900

    ✨ feat: 메인페이지 수정

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -19,20 +19,81 @@
 
     <!-- 방 목록-->
     <div
-      class="bottom-container flex flex-col gap-5 w-375 h-803 pt-3 pr-15 bg-white rounded-t-lg"
+      class="bottom-container flex flex-col gap-5 w-375 h-803 pt-3 bg-white rounded-t-lg"
     >
       <h1
-        class="text-2xl font-bold p-4 sticky top-0 bg-white h-16 rounded-t-lg z-40"
+        class="text-2xl font-bold p-4 sticky top-0 bg-white h-12 rounded-t-lg z-40"
       >
         나의 방 목록
       </h1>
 
-      <div class="flex flex-col justify-center pl-6 pr-6 gap-3 space-y-4 z-0">
-        <div v-for="room in rooms" :key="room.title">
-          <RoomCard :rooms="room"></RoomCard>
+      <!--방 유형 선택 버튼-->
+      <div class="flex flex-row">
+        <div
+          v-show="selected1"
+          class="flex flex-row pl-6 pr-28 space-x-3 items-center drop-shadow-xl"
+        >
+          <img src="/img/maintap1.png" class="w-24 h-auto" @click="select1" />
+          <img
+            src="/img/maintap2_2.png"
+            class="w-16 h-8 justify-self-center"
+            @click="select2"
+          />
+          <img
+            src="/img/maintap3_2.png"
+            class="w-16 h-8 justify-self-end"
+            @click="select3"
+          />
         </div>
-        <div v-for="temproom in temprooms" :key="temproom.invite_code">
-          <TempRoomCard :temprooms="temproom"></TempRoomCard>
+        <div
+          v-show="selected2"
+          class="flex flex-row pl-6 pr-28 space-x-3 items-center drop-shadow-xl"
+        >
+          <img
+            src="/img/maintap1_2.png"
+            class="w-16 h-8 justify-self-start"
+            @click="select1"
+          />
+          <img src="/img/maintap2.png" class="w-24 h-auto" @click="select2" />
+          <img
+            src="/img/maintap3_2.png"
+            class="w-16 h-8 justify-self-end"
+            @click="select3"
+          />
+        </div>
+        <div
+          v-show="selected3"
+          class="flex flex-row pl-6 pr-28 space-x-3 justify-items-center items-center drop-shadow-xl"
+        >
+          <img
+            src="/img/maintap1_2.png"
+            class="w-16 h-8 justify-self-start"
+            @click="select1"
+          />
+          <img src="/img/maintap2_2.png" class="w-16 h-8" @click="select2" />
+          <img
+            src="/img/maintap3.png"
+            class="w-24 h-auto justify-self-end"
+            @click="select3"
+          />
+        </div>
+      </div>
+
+      <div class="flex flex-col justify-center pl-6 pr-6 gap-3 z-0">
+        <div v-if="selected1" class="gap-3 space-y-8 pt-3">
+          <div v-for="room in rooms" :key="room.title">
+            <RoomCard :rooms="room"></RoomCard>
+          </div>
+        </div>
+        <div v-else-if="selected2" class="gap-3 space-y-8 pt-3">
+          <div v-for="temproom in temprooms" :key="temproom.invite_code">
+            <TempRoomCard :temprooms="temproom"></TempRoomCard>
+          </div>
+        </div>
+        <div v-else-if="selected3" class="gap-3 space-y-8 pt-3">
+          <div v-for="room in rooms" :key="room.title">
+            <RoomCard :rooms="room"></RoomCard>
+          </div>
         </div>
       </div>
     </div>
@@ -100,6 +161,45 @@ const getTempRoomList = async (): Promise<void> => {
   }
 };
 
+const today: Date = new Date();
+
+const inProgressRooms = (rooms: any): any => {
+  return rooms.filter(
+    (room: any) => new Date(room.setting.end_date as string) > today,
+  );
+};
+const notProgressRooms = (rooms: any): any => {
+  return rooms.filter(
+    (room: any) => new Date(room.setting.end_date as string) < today,
+  );
+};
+
+const selected1: Ref<boolean> = ref(true);
+const selected2: Ref<boolean> = ref(false);
+const selected3: Ref<boolean> = ref(false);
+
+const select1 = async (): Promise<void> => {
+  selected1.value = true;
+  selected2.value = false;
+  selected3.value = false;
+  await getRoomList();
+  rooms.value = inProgressRooms(rooms.value);
+};
+
+const select2 = async (): Promise<void> => {
+  selected1.value = false;
+  selected2.value = true;
+  selected3.value = false;
+};
+
+const select3 = async (): Promise<void> => {
+  selected1.value = false;
+  selected2.value = false;
+  selected3.value = true;
+  await getRoomList();
+  rooms.value = notProgressRooms(rooms.value);
+};
+
 const isSupported = (): boolean => {
   return 'Notification' in window && 'serviceWorker' in navigator;
 };
@@ -112,6 +212,7 @@ onMounted(async () => {
   }
   await getRoomList();
   await getTempRoomList();
+  await select1();
 });
 </script>
 
