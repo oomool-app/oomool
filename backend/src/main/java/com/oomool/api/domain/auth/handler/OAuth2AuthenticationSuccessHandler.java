b536f533cddbca74c450c4284b429ddee8597a6d
정필모 <itsmo1031@gmail.com>
2024-02-09 22:48:16 +0900

    ✨ feat: add query params in redirect url

--- backend/src/main/java/com/oomool/api/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/oomool/api/domain/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,9 +1,6 @@
 package com.oomool.api.domain.auth.handler;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -52,19 +49,16 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         User user = userRepository.findByEmail(email)
             .orElseThrow(() -> new EntityNotFoundException("해당 유저를 찾을 수 없습니다"));
 
-        String userId = String.valueOf(user.getId());
-
-        String url = makeRedirectUrl(jwtService.generateToken(userId, email, role));
-
-        String token = jwtService.generateToken(userId, email, role); // 액세스 토큰
-
+        int userId = user.getId();
+        String token = jwtService.generateToken(String.valueOf(userId), email, role); // 액세스 토큰
+        String url = makeRedirectUrl(token, user);
         String refreshToken = jwtService.generateRefreshToken(email, role); // 리프레시 토큰 생성
 
         // redis에 저장
-        refreshTokenService.saveTokenInfo(userId, email, refreshToken, "Bearer " + token);
+        refreshTokenService.saveTokenInfo(String.valueOf(userId), email, refreshToken, "Bearer " + token);
 
         // 응답 헤더에 액세스 토큰 반환
-        addTokenToHeader(response, token, user);
+        addTokenToHeader(response, token);
 
         if (response.isCommitted()) {
             logger.debug("응답이 이미 커밋된 상태입니다. 리다이렉트를 수행할 수 없습니다");
@@ -76,39 +70,23 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     }
 
     // 응답 헤더에 'Authorization' 이름으로 액세스 토큰 전달
-    private void addTokenToHeader(HttpServletResponse response, String token, User user) throws
-        UnsupportedEncodingException {
+    private void addTokenToHeader(HttpServletResponse response, String token) {
 
         response.addHeader("Authorization", "Bearer " + token);
-        response.addHeader("userId", String.valueOf(user.getId()));
-        response.addHeader("userEmail", user.getEmail());
-
-        String nickname = user.getUsername();
-
-        // 한글은 응답 헤더에 담아줄 수 없으므로 인코딩해서 넘겨야 한다.
-        if (isKorean(nickname)) {
-            // 한글인 경우 UTF-8로 인코딩하여 추가
-            response.addHeader("nickname", URLEncoder.encode(nickname, StandardCharsets.UTF_8.toString()));
-            return;
-        }
-
-        response.addHeader("nickname", nickname);
     }
 
     /**
      * 리디렉션 대상 URL을 결정하는 메서드
      */
-    private String makeRedirectUrl(String token) {
-        return UriComponentsBuilder.fromUriString(redirectUrl)
-            .queryParam("token", token)
-            .build()
-            .toUriString();
-    }
+    private String makeRedirectUrl(String token, User user) {
 
-    /**
-     * 정규식을 사용하여 문자열에 한글이 포함되어 있는지 확인하는 메서드
-     */
-    private boolean isKorean(String str) {
-        return str.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*");
+        return UriComponentsBuilder.fromUriString(redirectUrl + "/oauth/kakao")
+            .queryParam("token", token)
+            .queryParam("userId", user.getId())
+            .queryParam("email", user.getEmail())
+            .queryParam("username", user.getUsername())
+            .build()
+            .encode()
+            .toUriString();
     }
 }
