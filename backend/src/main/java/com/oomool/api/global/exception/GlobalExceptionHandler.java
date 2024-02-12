58178f9d98cb152ba820db6686cab6b207b9e237
sejeong-park <tpwjd29@gmail.com>
2024-02-13 03:33:18 +0900

    ♻️ refactor: redefine the common response format at globalExceptionHandler
    - 글로벌 Exception Handler 적용

--- backend/src/main/java/com/oomool/api/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/oomool/api/global/exception/GlobalExceptionHandler.java
@@ -4,12 +4,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.oomool.api.global.dto.ApiResponse;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<ErrorResponse> handleBaseException(BaseException ex) {
+    public ResponseEntity<ApiResponse<?>> handleBaseException(BaseException ex) {
         return ResponseEntity.status(ex.getStatusCode().getStatusCode())
-            .body(new ErrorResponse(ex.getStatusCode().getStatusCode(), ex.getStatusCode().getMessage()));
+            .body(ApiResponse.error(ex.getMessage()));
     }
+
+    // FrontEnd에서 검증되어 넘어오기 때문에 fail 메서드는 사용하지 않는다.
+    //
+    // @ExceptionHandler(MethodArgumentNotValidException.class)
+    // public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
+    //     Map<String, String> errors = new HashMap<>();
+    //     ex.getBindingResult().getAllErrors().forEach((error) -> {
+    //         String fieldName = ((FieldError)error).getField();
+    //         String errorMessage = error.getDefaultMessage();
+    //         errors.put(fieldName, errorMessage);
+    //     });
+    //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.fail(errors));
+    // }
+
 }
