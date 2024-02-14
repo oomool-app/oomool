055c374f9ed8cce37f799a2425a3ffce616bbf89
hinngreen <hinngreen@naver.com>
2024-02-14 11:08:24 +0900

    ✨ feat: 방 생성 시 이전 입력 데이터 초기화

--- frontend/stores/makeRoom.ts
+++ frontend/stores/makeRoom.ts
@@ -16,11 +16,23 @@ export const useMakeRoomStore = defineStore('make', () => {
 
     return `${year}-${month}-${day}`;
   }
+
   const diffDate = (start: string, end: string): number => {
     const startDate = new Date(start);
     const endDate = new Date(end);
     const diff = endDate.getTime() - startDate.getTime();
     return diff / (1000 * 60 * 60 * 24) + 1;
   };
-  return { name, type, number, range, formatDate, diffDate };
+
+  function resetSetting(): void {
+    name.value = '';
+    type.value = '';
+    number.value = 3;
+    range.value = {
+      start: new Date(),
+      end: new Date(),
+    };
+  }
+
+  return { name, type, number, range, formatDate, diffDate, resetSetting };
 });
